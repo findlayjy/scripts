fdd31bb043b0ade3a33117ebf18099eda32d3f0b
findlayjy <jy.findlay@gmail.com>
2017-03-14 20:13:56 +0000

    Added csv2latex script

--- csv2latex.sh
+++ csv2latex.sh
@@ -1,4 +1,4 @@
-# Converts $1 (csv file) into LaTeX tabular code
+# Simple script for converting csv files into LaTeX tabular code
 # Outputs to the clipboard and to a bare .txt file with the same name as the .csv file
 
-sed -E 's/,/\&/g' $1 | sed 's/$/\\\\/g' |  tee >(pbcopy) > ${1/.csv/}.txt
+sed -E 's/\ *,\ */\ \&\ /g' "$1" | sed 's/$/\ \\\\/g' |  tee >(pbcopy) > ${1/.csv/}.txt
